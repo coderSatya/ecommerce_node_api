e01da2920bfd4dafef2e035cf330c953106ff334
Satya Prakash <satya.prakash@webskitters.in>
2023-11-14 19:36:49 +0530

    connected to database

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+
+const connectDB = require('./db/connect')
 
 const PORT = process.env.PORT || 5000
 
@@ -17,6 +20,7 @@ app.use("/api/products", products_routes)
 
 const start = async()=>{
     try{
+        await connectDB(process.env.MONGODB_URL);
         app.listen(PORT, ()=>{
          console.log(`${PORT} YES I am connected`);
         });
--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,26 @@
-const getAllProducts = async(req, res)=>{
-res.status(200).json({msg:"I am getAllProducts"});
-}
+const Product = require("../models/product");
 
-const getAllProductsTesting = async(req, res)=>{
-res.status(200).json({msg:"I am getAllProductsTesting"})
+const getAllProducts = async (req, res) => {
+  const { name } = req.query;
+  const queryObject = {};
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  const myData = await Product.find(queryObject);
+  console.log(req.query);
+  res.status(200).json({ myData });
 };
-module.exports = {getAllProducts, getAllProductsTesting}
+
+const getAllProductsTesting = async (req, res) => {
+    const {sort} = req.query;
+    const queryObject = {};
+    let apiData = Product.find(queryObject);
+    if(sort){
+        let sortFix = sort.replace(","," ");
+        apiData = apiData.sort(sortFix)
+    }
+   
+ const myData = await apiData
+  res.status(200).json({myData})
+};
+module.exports = { getAllProducts, getAllProductsTesting };
